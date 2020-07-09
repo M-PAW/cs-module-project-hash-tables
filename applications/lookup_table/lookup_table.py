f45090ea96d254c1aa884afd726a3fe7fec95d6b
Michael Walter <michael@MacBook-Pro.local>
2020-07-09 13:44:54 -0700

    Completed lokup_table.py, starting on expensive_sequence.py

--- applications/lookup_table/lookup_table.py
+++ applications/lookup_table/lookup_table.py
@@ -1,4 +1,6 @@
 # Your code here
+import random
+import math
 
 
 def slowfun_too_slow(x, y):
@@ -9,12 +11,33 @@ def slowfun_too_slow(x, y):
 
     return v
 
+# create a cache
+slowfun_cache = {}
+
 def slowfun(x, y):
     """
     Rewrite slowfun_too_slow() in here so that the program produces the same
     output, but completes quickly instead of taking ages to run.
     """
     # Your code here
+
+    # define value of v
+    v = math.pow(x, y)
+    
+    # check if is already in cache
+    if v in slowfun_cache:
+        v = slowfun_cache[v]
+
+    # else perform calcs
+    else:
+        slowfun_cache[v] = math.factorial(v)
+        slowfun_cache[v] //= (x + y)
+        slowfun_cache[v] %= 982451653
+        # utilize recursion goodness with a reassignment
+        v = slowfun_cache[v]
+    # return v
+    return v
+
 
 
 
