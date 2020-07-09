294f175dcec942830ea2891652ef51bddfa0b694
Michael Walter <michael@MacBook-Pro.local>
2020-07-09 14:03:24 -0700

    Completed no_dups.py, still working on expensive_sequence.py

--- applications/expensive_seq/expensive_seq.py
+++ applications/expensive_seq/expensive_seq.py
@@ -1,8 +1,14 @@
 # Your code here
 
+hash_table = {}
 
 def expensive_seq(x, y, z):
     # Your code here
+    if x <= 0:
+        hash_table += y+z
+    elif x > 0:
+        hash_table += exp(x-1,y+1,z) + exp(x-2,y+2,z*2) + exp(x-3,y+3,z*3)
+    return hash_table
 
 
 
--- applications/no_dups/no_dups.py
+++ applications/no_dups/no_dups.py
@@ -1,6 +1,25 @@
 def no_dups(s):
     # Your code here
 
+    key_list = []
+
+    # Check for for no char and return
+    if s == '':
+        return ''
+
+    # Divide by spaces
+    listed = s.split(' ')
+
+    # Create a dictionary from listed
+    dicto = dict.fromkeys(listed)
+
+    # iterate through keys and add them to key list
+    for key in dicto.keys():
+        key_list.append(key)
+
+    # return keys list with single spacing between words, use .join()
+    return ' '.join(key_list) 
+
 
 
 if __name__ == "__main__":
